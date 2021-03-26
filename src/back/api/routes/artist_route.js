bf68f19e6c89e620a0eff6a333631e86decf869f
AMA <alexmarie78@gmail.com>
2021-03-26 16:56:39 +0100

    Majority of artist routes.

--- src/back/api/routes/artist_route.js
+++ src/back/api/routes/artist_route.js
@@ -10,7 +10,16 @@ module.exports = (config) => {
     router.post('/', (req, res) => {
         let connection = dbManager.OpenConnection(config);
         let artist = new artist_entity.Artist(req.body.name);
-        artist_model.InsertArtist(connection, artist);
+        artist_model.InsertArtist(connection, artist).then((artist_created) => {
+            if (artist_created['affectedRows'] !== 0) {
+                res.status(200).send(`Artist ${artist.name} has been created.`);
+            }
+            else {
+                res.status(400).send(`Artist ${artist} cannot be created. Pseudo is mandatory.`);
+            }
+        }).catch((error) => {
+            res.status(500).send(error);
+        });
     });
 
     // To get all artist
@@ -19,14 +28,26 @@ module.exports = (config) => {
 
         if(req.query.name != null ){
             // ex : "/artist?name=toto"
-            let artists = artist_model.GetArtistByName(connection,req.query.name);
-            artists.then(function(result){
-                console.log(result);
+            artist_model.GetArtistByName(connection,req.query.name).then( (artist) => {
+                if(artist != null){
+                    res.status(200).send(artist);
+                }
+                else {
+                    res.status(400).send(`Can not get ${artist.name}.`);
+                }
+            }).catch( (error) => {
+                res.status(500).send(error);
             })
         }else{
-            let artists = artist_model.GetAllArtists(connection)
-            artists.then(function(result){
-                console.log(result);
+            artist_model.GetAllArtists(connection).then( (artists) => {
+                if(artists != null){
+                    res.status(200).send(artists);
+                }
+                else {
+                    res.status(400).send(`Can not get artists, there is none.`);
+                }
+            }).catch( (error) => {
+                res.status(500).send(error);
             })
         }
 
@@ -35,9 +56,15 @@ module.exports = (config) => {
     // Get artist identified by his id
     router.get('/:id', (req, res) => {
         let connection = dbManager.OpenConnection(config);
-        let artists = artist_model.GetArtistById(connection,req.params.id);
-        artists.then(function(result){
-            console.log(result);
+        artist_model.GetArtistById(connection,req.params.id).then( (artist) => {
+            if(artist != null){
+                res.status(200).send(artist);
+            }
+            else {
+                res.status(400).send(`Can not get ${artist.name}.`);
+            }
+        }).catch( (error) => {
+            res.status(500).send(error);
         })
     });
 
@@ -55,7 +82,16 @@ module.exports = (config) => {
     // Delete artist identified by id
     router.delete('/:id', (req, res) => {
         let connection = dbManager.OpenConnection(config);
-        artist_model.DeleteArtist(connection, req.params.id);
+        artist_model.DeleteArtist(connection, req.params.id).then( (artist_deleted) => {
+            if (artist_deleted['affectedRows'] !== 0) {
+                res.status(200).send(`Artist ${req.params.id} has been deleted.`);
+            }
+            else {
+                res.status(400).send(`Artist ${req.params.id} not found. Can not delete it.`);
+            }
+        }).catch((error) => {
+            res.status(500).send(error);
+        })
     });
 
     return router;
