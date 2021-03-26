b6423d077f6c2be447a5dab812558a514e682226
AMA <alexmarie78@gmail.com>
2021-03-26 16:27:23 +0100

    Majority of track routes are done with status code.

--- src/back/api/routes/track_route.js
+++ src/back/api/routes/track_route.js
@@ -12,34 +12,59 @@ module.exports = (config) => {
         let track = new track_entity.Track(req.body.artist, req.body.danceability,
             req.body.duration, req.body.energy, req.body.instrumentalness,
             req.body.liveness, req.body.name, req.body.popularity, req.body.year);
-        track_model.InsertTrack(connection, track);
-        console.log("track POST /");
+        track_model.InsertTrack(connection, track).then((created) => {
+            if (created['affectedRows'] !== 0) {
+                res.status(200).send(`User ${track.title} has been created.`);
+            }
+            else {
+                res.status(400).send(`User ${track} cannot be created. Pseudo is mandatory.`);
+            }
+        }).catch((error) => {
+            res.status(500).send(error);
+        });
     });
 
     // To get all track
     router.get('/', (req, res) => {
         let connection = dbManager.OpenConnection(config);
-        if(req.query.name != null ){
+        if(req.query.title != null ){
             // ex : "/track?name=toto"
-            let tracks = track_model.GetTrackByName(connection,req.query.name);
-            tracks.then(function(result){
-                console.log(result);
+            track_model.GetTrackByName(connection,req.query.title).then((result) => {
+                if(result != null){
+                    res.status(200).send(result);
+                }
+                else{
+                    res.status(400).send(`Track ${req.query.title} not found in database.`);
+                }
+            }).catch((error) => {
+                res.status(500).send(error);
             })
         }else {
             track_model.GetAllTracks(connection).then((tracks) => {
-                res.status(200).send(tracks)
+                if(tracks != null){
+                    res.status(200).send(tracks);
+                }
+                else{
+                    res.status(400).send(`There is no tracks on database.`);
+                }
+            }).catch((error) => {
+                res.status(500).send(error);
             })
-            console.log("track GET /");
         }
     });
 
     // Get track by id
     router.get('/:id', (req, res) => {
-        console.log("track GET /:id => id = "+req.params.id);
         let connection = dbManager.OpenConnection(config);
-        let tracks = track_model.GetTrackById(connection,req.params.id);
-        tracks.then(function(result){
-            console.log(result);
+        track_model.GetTrackById(connection,req.params.id).then((result) => {
+            if (result != null){
+                res.status(200).send(result);
+            }
+            else {
+                res.status(400).send(`The song ${req.params.id} does not exist in the database.`);
+            }
+        }).catch((error) => {
+            res.status(500).send(error);
         })
     });
 
@@ -54,15 +79,30 @@ module.exports = (config) => {
             req.body.energy, req.body.instrumentalness, req.body.liveness,
             req.body.name, req.body.popularity, req.body.year)
         //new_track.sreq.body.name, req.body.year, req.body.duration);
-        track_model.UpdateTrack(connection, req.params.id, new_track);
-        console.log("track PUT /:id => id = "+req.params.id);
+        track_model.UpdateTrack(connection, req.params.id, new_track).then((result) => {
+                if (result != null) {
+                    res.status(200).send(`Track ${result} has been created.`);
+                } else {
+                    res.status(400).send(`Track has not been created, something is missing.`);
+                }
+            }).catch((error) => {
+                res.status(500).send(error);
+            })
     });
 
     // To delete a track identified by id
     router.delete('/:id', (req, res) => {
         let connection = dbManager.OpenConnection(config);
-        track_model.DeleteTrack(connection, req.params.id);
-        console.log("track DELETE /:id => id = "+req.params.id);
+        track_model.DeleteTrack(connection, req.params.id).then( (deleted) => {
+            if (deleted['affectedRows'] !== 0) {
+                res.status(200).send(`Track ${req.params.id} has been deleted.`);
+            }
+            else {
+                res.status(400).send(`Track ${req.params.id} not found. Can not delete it.`);
+            }
+        }).catch((error) => {
+            res.status(500).send(error);
+        })
     });
 
     return router;
